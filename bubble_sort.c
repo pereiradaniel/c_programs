a4f381ffcae5dce429333ba1643484c7ff6d4250
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-09-21 15:11:57 -0400

    Optimized

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-
 void swap(int *x, int *y)
 {
     // compares and swaps if necessary
-    if (*x > *y)
+    while (*x > *y)
     {
         printf("Comparing %d and %d\n", *x, *y);
         int temp = *y;
@@ -14,18 +13,30 @@ void swap(int *x, int *y)
     return;
 }
 
+void bubble_sort(int a[], int length)
+{
+   for (int i = 0; i < length; ++i)
+        for (int j = 0; j < length - 1; ++j)
+            swap(&a[j], &a[j+1]);
+}
+
+void display(int x[], int length)
+{
+    for (int i=0; i < length; ++i)
+        printf("%d\n", x[i]);
+}
+
 int main()
 {
     int a[] = {7,1,3,9,0,2,4,5,8};
-
+    int b[] = {12,345,12,456,23,34,546,2,34,5,92,74,2829,4729,273,84721,372};
     int length = sizeof(a) / sizeof(int);
 
-    for (int i = 0; i < length; ++i)
-        for (int j = 0; j < length - 1; ++j)
-            swap(&a[j], &a[j+1]);
+    bubble_sort(a, length);
+    bubble_sort(b, sizeof(b) / sizeof(int));
 
-    for (int i=0; i<length; ++i)
-        printf("%d\n", a[i]);
+    display(a, length);
+    display(b, sizeof(b) / sizeof(int));
 
     return 0;
 }

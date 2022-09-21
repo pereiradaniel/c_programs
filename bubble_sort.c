5bbc4c40587f0b447b65278f195b61ff8c9f51b5
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-09-21 14:59:22 -0400

    Works.

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+
+
+void swap(int *x, int *y)
+{
+    // compares and swaps if necessary
+    if (*x > *y)
+    {
+        printf("Comparing %d and %d\n", *x, *y);
+        int temp = *y;
+        *y = *x;
+        *x = temp;
+    }
+    return;
+}
+
 int main()
 {
-    int a[] = {7,1,3,9,0,1,4,5,8};
+    int a[] = {7,1,3,9,0,2,4,5,8};
+
+    int length = sizeof(a) / sizeof(int);
+
+    for (int i = 0; i < length; ++i)
+        for (int j = 0; j < length - 1; ++j)
+            swap(&a[j], &a[j+1]);
+
+    for (int i=0; i<length; ++i)
+        printf("%d\n", a[i]);
 
     return 0;
 }

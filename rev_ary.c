5eb1a36516a95255b96c00c7c8d5cb93514c7445
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-09-22 15:59:38 -0400

    Finished.

--- rev_ary.c
+++ rev_ary.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
-int main()
-{
-    int array[] = {5,6,7,8,9};
+#define LENGTH(a) sizeof(a) / sizeof(a[0]) - 1 // function-like macro
 
-    for (int i = sizeof(array) / sizeof(array[0])-1; i>=0; --i)
+void print_reverse(int array[], int length)
+{
+    for (int i = length; i>=0; --i)
         printf("%d\n", array[i]);
+}
+
+int main(int argc, char*argv[])
+{
+    int a[] = {5,6,7,8,9};
+    int b[] = {20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0};
+
+    print_reverse(a, LENGTH(a));
+    print_reverse(b, LENGTH(b));
     
     return 0;
 }

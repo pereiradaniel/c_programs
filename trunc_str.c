0cd47922227ce6c4519fd31299f49baff12fe866
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-09-24 15:32:17 -0400

    trunc_str function added

--- trunc_str.c
+++ trunc_str.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+
+// when an array is passed to a function, it is passed a pointer!
+// the pointer stores the memory address of the FIRST element of
+// the array!
+void tr_str(char string[], int n) {
+    string[n] = '\0';
+};
+
 int main(int argc, char* argv[]) {
 
     char string[] = "abcdefg"; // there is a null terminator added to the char array!
 
     printf("%s\n", string);
-    string[5] = '\0'; // this will truncate the string!
-    printf("%s\n", string);
+
+    char quote[] = "abc def ghi jkl mno";
+
+    printf("quote before: %s\n", quote);
+    tr_str(quote, 10);
+    printf("quote after: %s\n", quote);
 
     return 0;
 }

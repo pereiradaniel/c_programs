33e829577e238f3030e4dd9640ed45dfc587af46
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-09-24 15:25:07 -0400

    string truncated with null terminator

--- trunc_str.c
+++ trunc_str.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 int main(int argc, char* argv[]) {
 
+    char string[] = "abcdefg"; // there is a null terminator added to the char array!
+
+    printf("%s\n", string);
+    string[5] = '\0'; // this will truncate the string!
+    printf("%s\n", string);
+
     return 0;
 }

bb77f87b751f887bac4de06614ea29b07db92764
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-09-26 15:18:24 -0400

    Insertion sort works

---
+++ insertion_sort.c
@@ -0,0 +1,34 @@
+// Implement an insertion sort
+#include <stdio.h>
+void insertion_sort(int a[], int length);
+
+int main(int argc, char* argv[])
+{
+    int a[] = {8,4,9,5,7,6,3,2};
+    insertion_sort(a,8);
+    for (int i = 0; i<8; ++i)
+    {
+        printf("a[%d] = %d\n", i, a[i]);
+    }
+    return 0;
+
+}
+
+void insertion_sort(int a[], int length)
+{
+    for (int i = 1; i < length; ++i) // no need to sort 0 elem, so i=1
+    {
+        int key = a[i]; // keep track of curr elem
+        int j = i - 1;
+
+        // as long as a[j] >, keep shifting elems
+        while(j >= 0 && a[j] > key)
+        {
+            a[j+1] = a[j];
+            j = j-1;
+        }
+
+        // Insert value at correct locn
+        a[j+1] = key;
+    }
+}

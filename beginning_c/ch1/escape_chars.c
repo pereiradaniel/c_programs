089245d7b80975bdbbec9b77753a420764c17a50
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-09-28 16:24:40 -0400

    rename dir

---
+++ beginning_c/ch1/escape_chars.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    printf("TABLE\n");
+    printf("\t1. Item one\n");
+    printf("\t2. Item two\n");
+    printf("\t3. Item three\n");
+    printf("\"double quotes\"\n");
+    printf("\aAlert!\n");
+
+    return 0;
+}

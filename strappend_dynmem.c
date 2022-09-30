3f087cffc2747778471f3612b72499c8f7aae486
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-09-30 13:47:47 -0400

    Passes valgrind

--- strappend_dynmem.c
+++ strappend_dynmem.c
@@ -41,3 +41,19 @@ int main(int argc, char* argv[])
 
     return 0;
 }
+
+// ==160== Memcheck, a memory error detector
+// ==160== Copyright (C) 2002-2017, and GNU GPL'd, by Julian Seward et al.
+// ==160== Using Valgrind-3.15.0 and LibVEX; rerun with -h for copyright info
+// ==160== Command: ./a.out
+// ==160== 
+// s: abcdefghij
+// ==160== 
+// ==160== HEAP SUMMARY:
+// ==160==     in use at exit: 0 bytes in 0 blocks
+// ==160==   total heap usage: 2 allocs, 2 frees, 1,035 bytes allocated
+// ==160==
+// ==160== All heap blocks were freed -- no leaks are possible
+// ==160==
+// ==160== For lists of detected and suppressed errors, rerun with: -s
+// ==160== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)

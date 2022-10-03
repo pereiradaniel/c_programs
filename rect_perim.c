bd586274fdb400fd0541cca7a90951a4b884bbb8
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-10-03 10:13:26 -0400

    Update function

--- rect_perim.c
+++ rect_perim.c
@@ -1,32 +1,39 @@
 // Calculate the perimeter of a rectangle using user input.
 #include <stdio.h>
+
+//  Returns the perimeter of a rectangle.
+double rect_perim(double l, double w)
+{
+    return 2*(l+w);
+}
+
 int main(int arg, char* argv[])
 {
-    double length, width, perimeter;
+    double length, width; // perimeter;
     printf("Length: ");
     scanf("%lf", &length);
     printf("Width: ");
     scanf("%lf", &width);
-    perimeter = 2*(length+width);
-    printf("Perimeter: %.2f\n", perimeter);
+    // perimeter = rect_perim(length, width);
+    printf("Perimeter: %.2f\n", rect_perim(length, width));
 
     return 0;
 }
 
-// ==186== Memcheck, a memory error detector
-// ==186== Copyright (C) 2002-2017, and GNU GPL'd, by Julian Seward et al.
-// ==186== Using Valgrind-3.15.0 and LibVEX; rerun with -h for copyright info
-// ==186== Command: ./a.out
-// ==186==
-// Length: 3
-// Width: 7
-// Perimeter: 20.00
-// ==186==
-// ==186== HEAP SUMMARY:
-// ==186==     in use at exit: 0 bytes in 0 blocks
-// ==186==   total heap usage: 2 allocs, 2 frees, 2,048 bytes allocated
-// ==186==
-// ==186== All heap blocks were freed -- no leaks are possible
-// ==186==
-// ==186== For lists of detected and suppressed errors, rerun with: -s
-// ==186== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)
+// ==132== Memcheck, a memory error detector
+// ==132== Copyright (C) 2002-2017, and GNU GPL'd, by Julian Seward et al.
+// ==132== Using Valgrind-3.15.0 and LibVEX; rerun with -h for copyright info
+// ==132== Command: ./a.out
+// ==132==
+// Length: 25
+// Width: 15
+// Perimeter: 80.00
+// ==132==
+// ==132== HEAP SUMMARY:
+// ==132==     in use at exit: 0 bytes in 0 blocks
+// ==132==   total heap usage: 2 allocs, 2 frees, 2,048 bytes allocated
+// ==132==
+// ==132== All heap blocks were freed -- no leaks are possible
+// ==132==
+// ==132== For lists of detected and suppressed errors, rerun with: -s
+// ==132== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)

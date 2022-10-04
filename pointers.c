66f887ad9caef0397afa9379a3bcf8a0bd034de5
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-10-04 15:17:31 -0400

    Rename file

--- pointers.c
+++ pointers.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 int main(int arg, char* argv[])
 {
-    
+    int b = 42;
+
+    // Make int *a point to b;
+    int *a = &b;
+
+    printf(" b: %d\n", b);  // value stored by b: 42
+    printf("&b: %p\n", &b); // mem address for b
+    printf(" p: %p\n", a);  // mem address for b
 
     return 0;
 }

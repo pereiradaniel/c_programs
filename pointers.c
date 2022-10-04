c99570e2d3e2c6135972def478ccdbbe71ecc2b0
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-10-04 15:20:14 -0400

    Dereference pointer

--- pointers.c
+++ pointers.c
@@ -10,5 +10,14 @@ int main(int arg, char* argv[])
     printf("&b: %p\n", &b); // mem address for b
     printf(" p: %p\n", a);  // mem address for b
 
+    // * allows dereferencing of pointer: Access the value at in the mem address
+    // stored at the pointer. Access the data being pointed to.
+
+    *a = 50; // Will set b to 50 because of dereferencing!
+
+    printf(" b: %d\n", b);  // value stored by b: 50
+    printf("&b: %p\n", &b); // mem address for b
+    printf(" p: %p\n", a);  // mem address for b
+
     return 0;
 }

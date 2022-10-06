5a5be94f6c396ab748f0f5d2a053a56bbf0e3e0c
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-10-05 20:19:11 -0400

    const ptrs

--- consts_ptrs.c
+++ consts_ptrs.c
@@ -1,7 +1,24 @@
+// Illustrate the differences between constant ptr and ptr to a constant
 #include <stdio.h>
 int main(int argc, char* argv[])
 {
+    char a = 'a';
+    char b = 'b';
     
-    
+    char*const const_ptr = &a;     // constant pointer
+                                   // can't change what it points to
+                                   // can change the value
+    *const_ptr = 'x';
+    printf("a: %c\n", a);
+
+    char const *ptr_to_const = &a; // pointer to constant
+    printf("*ptr_to_const: %c\n", *ptr_to_const); // dereference ptr
+    ptr_to_const = &b;
+    printf("*ptr_to_const: %c\n", *ptr_to_const); // dereference ptr
+
+    const char *const const_ptr_to_const = &a;
+    // can't dereference this pointer because it is a pointer to a constant!
+    // can't change what the pointer points to!
+
     return 0;
 }

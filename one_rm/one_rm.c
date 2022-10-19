3b79759f62c38223dd516369901ec32f8867710f
pereiradaniel <danielpaulpereira@protonmail.com>
2022-10-18 21:46:39 -0400

    Add one rep max finder.

---
+++ one_rm/one_rm.c
@@ -0,0 +1,14 @@
+//  Based on user input, calculate and output a scale of 50%-90% of 1RM
+#include <stdio.h>
+int main(int argc, int argv[])
+{
+    double onerm = 0;
+    
+    printf("Enter your one rep max: \n");
+    scanf("%lf", &onerm);
+
+    for (int i=5; i <= 9; ++i)
+        printf("%.2f%% = %.2f lbs\n", i*0.1*100, i*0.1*onerm);
+
+    return 0;
+}

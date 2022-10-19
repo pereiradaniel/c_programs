e6380a52eb30b614c066104f1783bddc6afaa584
pereiradaniel <danielpaulpereira@protonmail.com>
2022-10-18 21:59:34 -0400

    complete

--- one_rm/one_rm.c
+++ one_rm/one_rm.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 int main(int argc, int argv[])
 {
-    double onerm = 0;
+    double onerm = 0; // stores user input for one rep max
     
     printf("Enter your one rep max: \n");
     scanf("%lf", &onerm);
-
-    for (int i=5; i <= 9; ++i)
+    
+    for (int i=5; i <= 9; ++i) // Loop from 50% to 90% of one rep max
         printf("%.2f%% = %.2f lbs\n", i*0.1*100, i*0.1*onerm);
 
     return 0;

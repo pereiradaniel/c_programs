d2ff4f2d569c4d0064541fdcd575992296329f4f
Daniel Pereira <danielpaulpereira@protonmail.com>
2023-01-09 21:34:01 -0500

    sums

---
+++ sum/sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "../length.h"
+
+int main(int argc, char* argv)
+{
+    int sum = 0;
+    int nums[] = {1,1,1};
+
+    for (int i = 0; i < LENGTH(nums); ++i)
+        sum = sum + nums[i];
+
+    printf("\nSum total = %d\n", sum);
+    
+    return 0;
+}

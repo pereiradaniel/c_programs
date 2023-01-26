f38ada3ffb56fcf4a2e9170387649dcf461ae5c9
Daniel Pereira <danielpaulpereira@protonmail.com>
2023-01-26 17:08:06 -0500

    Complete

--- bin_dec_conv/dual_conversion/dual_conv.c
+++ bin_dec_conv/dual_conversion/dual_conv.c
@@ -1,55 +1,46 @@
 // Convert decimal to binary or binary to decimal
-
 #include <stdio.h>
 #include <stdbool.h>
 #include <ctype.h>
 #include <math.h>
 
-void binToDec(int n);
-void decToBin(int n);
+void binToDec(int n); // Convert binary to decimal
+void decToBin(int n); // Convert decimal to binary
 
 int main(int argc, char* argv[])
 {
-    int users_number = 0;
-    int choice = 0;
-    bool main_loop = true;
-    char repeat = 'Y';
+    int users_number = 0;   // Number to convert
+    int choice = 0;         // Selects binary or decimal conversion
+    bool main_loop = true;  // Breaks main loop on choice
+    char repeat = 'Y';      // User repeats program
+
+    // Print program heading:
+    printf("\nBinary to decimal converter\n");
+    printf("\n---------------------------\n");
 
     while (main_loop)
     {
-        // Print program heading:
-        printf("\nBinary to decimal converter\n");
-        printf("\n---------------------------\n");
-
         // Ask user to convert decimal or binary number:
         while(choice < 1 || choice > 2)
         {
             printf("\nConvert a decimal or binary number?\n\t1 - Decimal\n\t2 - Binary\n");
             if (scanf(" %d", &choice) != 1)
                 printf("\nFailed to read integer!");
+            
+            if (choice < 1 || choice > 2)
+                printf("\nSelection is out of bounds! Choose 1 or 2.\n");
         }
 
+        // Get user's number to convert, display correct prompt:
         printf("\nEnter %s: ", (choice == 1 ? "an integer": "a binary number"));
         if (scanf(" %d", &users_number) != 1)
             printf("\nFailed to read integer!");
 
-
-        if (choice == 1) // Decimal to binary selected
-        {
-            // printf("\nEnter an integer: ");
-            // if (scanf(" %d", &users_number) != 1)
-            //     printf("\nFailed to read integer!");
-            
+        // Choose conversion method:
+        if (choice == 1)            // Decimal to binary selected
             decToBin(users_number);
-        }
-        else if (choice ==2) // Binary to decimal selected
-        {
-            // printf("\nEnter a binary number: ");
-            // if (scanf(" %d", &users_number) != 1)
-            //     printf("\nFailed to read integer!");
-
+        else if (choice ==2)        // Binary to decimal selected
             binToDec(users_number);
-        }
 
         // Ask user to repeat program:
         printf("\nAgain? (Y/N)\n");
@@ -64,11 +55,8 @@ int main(int argc, char* argv[])
             users_number = 0;
             choice = 0;
         }
-        
     }
-
-    
-
+    // End program:
     return 0;
 }
 

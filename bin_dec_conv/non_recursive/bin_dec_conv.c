ed46ae3edaad5a4a4263456cbb581a612f33bb8d
Daniel Pereira <danielpaulpereira@protonmail.com>
2023-01-27 17:18:10 -0500

    Edit

--- bin_dec_conv/non_recursive/bin_dec_conv.c
+++ bin_dec_conv/non_recursive/bin_dec_conv.c
@@ -22,12 +22,9 @@ void decToBin(int n)
 {
     // Declare and fill array (will be backwards):
     int binary[32] = {0};
-    int i=0;
-    for (; n > 0; ++i)
-    {
+    int i=0; // Re-use this counter when printing
+    for (; n > 0; ++i, n /= 2)
         binary[i] = n % 2;
-        n /= 2;
-    }
 
     // Print array in reverse order:
     for (int j=i-1; j >= 0; --j)

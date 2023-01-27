ad5e80794dca7b7d014dd9cf1b5de9266d0be628
Daniel Pereira <danielpaulpereira@protonmail.com>
2023-01-26 19:15:02 -0500

    Add comments and clean up.

--- bin_dec_conv/recursive/bin_dec_conv.c
+++ bin_dec_conv/recursive/bin_dec_conv.c
@@ -1,16 +1,22 @@
 // Convert decimal to binary using a recursive function.
+
 #include <stdio.h>
-int decToBin(const int number);
+
+int decToBin(const int number); // Returns binary conversion.
+
 int main(int argc, char* argv[])
 {
-    int users_number = 0;
+    int users_number = 0;   // Number to convert.
+
     // Get user input:
     printf("\nEnter an integer: ");
     if (scanf(" %d", &users_number) != 1)
         printf("\nFailed to read integer!");
     
+    // Convert to binary:
     int binary = decToBin(users_number);
 
+    // Output result:
     printf("\n%d = %d\n", users_number, binary);
 
     return 0;
@@ -18,9 +24,11 @@ int main(int argc, char* argv[])
 
 int decToBin(const int number)
 {
-    int result = 0;
+    int result = 0; // Default is zero. (Prevents division by 0).
+
+    // If number is not 0, perform conversion.
     if (number != 0)
         result = (number % 2) + 10 * decToBin(number / 2);
+    
     return result;
-
 }

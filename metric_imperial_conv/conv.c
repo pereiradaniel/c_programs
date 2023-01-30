8fcf8aa3c5bbe6c224937cb6903ee2b434e15e48
Daniel Pereira <danielpaulpereira@protonmail.com>
2023-01-30 17:06:51 -0500

    Ready to test

--- metric_imperial_conv/conv.c
+++ metric_imperial_conv/conv.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
+double cmToInch(const double n);
+
 int main(int argc, char* argv)
 {
-    int user_input = 0;
-    
+    double user_input = 0;
+
     printf("\nEnter length in cm:");
-    if (scanf(" %d", &user_input) != 1)
-        printf("\nFailed to read integer!\n");
+    if (scanf(" %lf", &user_input) != 1)
+        printf("\nFailed to read input!\n");
+
+    printf("\n%.2lf cm = %.2lf inches", user_input, cmToInch(user_input));
 
     return 0;
+}
+
+double cmToInch(const double n)
+{
+    return n*0.393700787;
 }
